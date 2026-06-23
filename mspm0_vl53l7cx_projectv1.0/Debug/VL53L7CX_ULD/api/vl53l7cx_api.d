b0b13859b7b254655d3a3232446660b327b75bae
25DanSa <danijel.sabic@vusion.com>
2026-06-23 11:10:19 +0200

    Delete mspm0_vl53l7cx_projectv1.0 directory

--- mspm0_vl53l7cx_projectv1.0/Debug/VL53L7CX_ULD/api/vl53l7cx_api.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-VL53L7CX_ULD/api/vl53l7cx_api.o: ../VL53L7CX_ULD/api/vl53l7cx_api.c \
- ../VL53L7CX_ULD/api/vl53l7cx_api.h \
- C:/Users/dsabic/workspace_ccstheia/mspm0_vl53l7cx_projectv1.0/VL53L7CX_ULD/platform/platform.h \
- ../VL53L7CX_ULD/api/vl53l7cx_buffers.h
-../VL53L7CX_ULD/api/vl53l7cx_api.h:
-C:/Users/dsabic/workspace_ccstheia/mspm0_vl53l7cx_projectv1.0/VL53L7CX_ULD/platform/platform.h:
-../VL53L7CX_ULD/api/vl53l7cx_buffers.h:
--- mspm0_vl53l7cx_projectv1.0/Debug/VL53L7CX_ULD/platform/platform.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-VL53L7CX_ULD/platform/platform.o: ../VL53L7CX_ULD/platform/platform.c \
- ../VL53L7CX_ULD/platform/platform.h
-../VL53L7CX_ULD/platform/platform.h:
